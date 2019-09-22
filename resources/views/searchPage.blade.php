e0f57ab82f6a6dfb7e4ac174f1d8bbc1678dc60f
rubandass jhondass <jhonr1@student.op.ac.nz>
2019-09-23 11:39:16 +1200

    Final push

--- resources/views/searchPage.blade.php
+++ resources/views/searchPage.blade.php
@@ -5,27 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css')}}">
     <title>Search</title>
 </head>
 
 <body>
-    <div class="mt-5">
-        <form class="text-center" method="post" action="/showPlayers">
-            {{csrf_field()}}
-            <div class="container input-group col-md-4">
-                <select class="custom-select" name="role">
-                    <option selected>Show All</option>
-                    @foreach ($roles as $role){
-                    <option value="{{$role}}">{{$role}}</option>
-                    @endforeach
-                </select>
-                <div class="input-group-append">
-                    <button type="submit" class="btn btn-primary" name="submit">Search</button>
-                </div>
-            </div>
-        </form>
-    </div>
+	<form class="mt-5" method="post" action="{{url('/showPlayers')}}">
+	{{csrf_field()}}
+		<div class="container form-group offset-md-4">
+			<label for="roles">Select Cricket players role</label>
+			<div class="input-group">
+				<select class="form-control custom-select col-md-4" name="role" id="roles">
+					<option selected>Show All</option>
+					@foreach ($roles as $role){
+					<option value="{{$role}}">{{$role}}</option>
+					@endforeach
+				</select>
+				<div class="input-group-append">
+					<button type="submit" class="btn btn-primary" name="submit">Search</button>
+				</div>
+				
+			</div>
+			
+		</div>
+	
+	</form>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
--- resources/views/showPlayersPage.blade.php
+++ resources/views/showPlayersPage.blade.php
@@ -5,46 +5,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="/css/mystyle.css">
+    <link rel="stylesheet" href="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('/css/mystyle.css')}}">
     <title>Show Results</title>
 </head>
 
 <body>
-    <table class="table table-bordered">
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Role</th>
-            <th>Batting</th>
-            <th>Bowling</th>
-            <th>Image</th>
-            <th>ODI Runs</th>
-            <th>Country</th>
-        </tr>
+	<div class="container-fluid col-md-10 offset-md-1 mt-2">
 
-        @foreach($result as $player)
-        <tr>
-            <td>{{$player->id}}</td>
-            <td>{{$player->name}}</td>
-            <td>{{$player->age}}</td>
-            <td>{{$player->role}}</td>
-            <td>{{$player->batting}}</td>
-            <td>{{$player->bowling}}</td>
-            <td class="players-image"><img src="/images/{{$player->image}}" alt="image" style="width:50px; height:auto;"></td>
-            <td>{{$player->odiRuns}}</td>
-            <td>{{$player->country->name}}</td>
+		<h4 class="text-center text-dark">Cricket players information</h4>
+		<div class="row">
+			
+				@foreach($result as $player)
+			
+				<div class="col-md-4 space_div">
+				
+					<div class="card border-0 br-5" style="width: 18rem;">
+						<div class="ribbon">
+							<span class="ribbon1">
+							  <span>{{$player->country->name}}</span>
+							</span>
+						</div>
+						<img class="card-img-top" src="{{asset('images/' . $player->image)}}"  alt="{{$player->name}}">
+					
+						<div class="card-body">
+							<h5 class="card-title text-center text-dark">{{$player->name}} ({{$player->age}})<br/></h5>
+							<div class="row">
+						
+							 <ul class="list-group list-group-flush">
+								<li class="list-group-item"><b>Id:</b> {{$player->id}}</li>
+								<li class="list-group-item"><b>Role:</b> {{$player->role}}</li>
+								<li class="list-group-item"><b>Batting:</b> {{$player->batting}}</li>
+								<li class="list-group-item"><b>Bowling:</b> {{$player->bowling}}</li>
+								<li class="list-group-item"><b>ODI Runs:</b> {{$player->odiRuns}}</li>
+							 </ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				@endforeach
+			
+		</div>
 
-        </tr>
-        @endforeach
-    </table>
-    <div class="col-md-6">
-        <form method="post" action="/">
-            {{csrf_field()}}
-            <button class="btn btn-primary" type="submit" name="back">back</button>
-        </form>
-    </div>
+		<div class="space_div">
+			<form method="post" action="{{url('/')}}">
+				{{csrf_field()}}
+				<button class="btn btn-primary offset-md-6" type="submit" name="back">back</button>
+			</form>
+		</div>
+	</div>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
